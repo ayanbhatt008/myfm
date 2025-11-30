b9c95d0272dbfb165db75dd4b5d953c6afe2d17e
ayanbhatt-MSI <ayan.bhatt008@gmail.com>
2025-11-30 14:39:40 -0500

    set show_dialog to false
    Signed-off-by: ayanbhatt-MSI <ayan.bhatt008@gmail.com>

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+import {NextResponse} from "next/server";
 
 const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
 const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
@@ -7,12 +7,13 @@ const REDIRECT_URI = `${process.env.NEXT_BASE_URL}api/auth/callback`;
 const scope = "user-top-read";
 
 export async function GET() {
+    const show_dialog = "false"
     const params = new URLSearchParams({
         response_type: "code",
         client_id: CLIENT_ID!,
         scope: scope,
         redirect_uri: REDIRECT_URI,
-        show_dialog: "true"
+        show_dialog: show_dialog,
     });
 
     return NextResponse.redirect(`https://accounts.spotify.com/authorize?${params.toString()}`);
