2e58ede5d4aa4278837e9cfa88314ed9d49f0959
ayanbhatt-MSI <ayan.bhatt008@gmail.com>
2025-11-30 11:55:17 -0500

    optimized ensureAccessToken() to call to supabase api twice
    Signed-off-by: ayanbhatt-MSI <ayan.bhatt008@gmail.com>

--- lib/spotify.ts
+++ lib/spotify.ts
@@ -2,7 +2,7 @@ import {getSessionData} from "@/lib/session";
 import {supabase} from "@/lib/supabase";
 
 
-export async function getAccessToken() {
+export async function ensureAccessToken() {
     const session = await getSessionData();
     const internalUUID = session.internalUUID;
 
@@ -14,13 +14,43 @@ export async function getAccessToken() {
 
     const epoch = tokenData.expires_at;
 
-    if (epoch< Date.now()) {
-        await refreshToken(tokenData.refresh_token);
+    if (epoch > Date.now()) {
+        return;
     }
+    const refresh_token = tokenData.refresh_token;
+
+    const basicAuth = Buffer.from(`${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`).toString("base64");
+    const body = new URLSearchParams({
+        "grant_type": "refresh_token",
+        "refresh_token": refresh_token,
+    });
+
+    const res = await fetch("https://accounts.spotify.com/api/token",{
+        method: "POST",
+        headers: {
+            Authorization : `Basic ${basicAuth}`,
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: body
+    });
+
+
+    const data = await res.json();
+    console.log(data);
+
+    await supabase
+        .from("tokens")
+        .update({
+            access_token: data.access_token,
+            expires_at: Date.now() + data.expires_in * 1000
+        })
+        .eq("user_id", internalUUID);
+
+
 
 }
 
-async function refreshToken(refresh_token : string) {
+/*async function refreshToken(refresh_token : string) {
     const session = await getSessionData();
     const internalUUID = session.internalUUID;
 
@@ -53,4 +83,6 @@ async function refreshToken(refresh_token : string) {
         .eq("user_id", internalUUID);
 
 
-}
+}*/
+
+
