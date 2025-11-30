4c3feca27833ca1216102388f3b5249ddd7a09e1
ayanbhatt-MSI <ayan.bhatt008@gmail.com>
2025-11-30 14:37:56 -0500

    basic home page
    Signed-off-by: ayanbhatt-MSI <ayan.bhatt008@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,49 @@
 "use client";
+
+import React, {useState} from "react";
 import Image from "next/image";
-import React from "react";
-import { useState } from "react";
-//
 
 export default function Home() {
-  const [response, setResponse] = useState("def");
+    const [response, setResponse] = useState("def");
 
 
-  const handleSubmit = async() => {
-    
-    window.location.href  = "/api/auth/login";
-  }
+    const handleSubmit = async () => {
+
+        window.location.href = "/api/auth/login";
+    }
 
 
+    return (
+        <div
+            className="flex min-h-screen  items-start justify-center bg-zinc-50 font-sans text-black text-center max-w-screen-l">
+            <div className={"flex-1 items-start mt-40"}>
+                <h1 className={"font-bold text-3xl"}>My.FM</h1>
+                <div className={""}>
+                    hii
+                </div>
 
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans ">
-      <button onClick={handleSubmit} className = "px-4 py-2 bg-blue-500 text-white rounded"> {response} </button>
-      
-    </div>
-  );
+            </div>
+
+
+            <div
+                className={"flex-1 bg-[#191414] rounded-2xl p-10 border border-white/10 shadow-lg flex flex-col self-center items-center gap-6"}>
+                <a href={"/api/auth/login"} className={"flex flex-col items-center gap-10"}>
+                    <Image
+                        src={"/Spotify_Full_Logo_RGB_Green.png"}
+                        alt={"Spotify Logo"}
+                        width={200}
+                        height={200}
+                    />
+
+                    <div className="bg-[#1ED760] rounded-2xl p-5 gap-6 text-white">
+                        Log-In with Spotify
+                    </div>
+
+
+                </a>
+
+            </div>
+
+        </div>
+    );
 }
