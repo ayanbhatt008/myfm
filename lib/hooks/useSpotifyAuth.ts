7a0aaec676709952889655095a841e95bd725c6d
ayanbhatt-ACER <ayan.bhatt008@gmail.com>
2025-12-04 12:05:24 -0500

    useSpotifyAuth

---
+++ lib/hooks/useSpotifyAuth.ts
@@ -0,0 +1,33 @@
+import { supabase } from "../supabase/client";
+import {useState, useEffect} from "react";
+
+
+export function useSpotifyAuth() {
+    
+    const [loading, setLoading] = useState(true);
+    const [displayName, setDisplayName] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchSpotify = async () => {
+            const {data: {user}} = await supabase.auth.getUser();
+            if (!user)
+                return;
+
+            const {data: spotifyData} = await supabase
+                .from("spotify_tokens")
+                .select("display_name")
+                .eq("user_id", user?.id)
+                .maybeSingle();
+            
+            setDisplayName(spotifyData?.display_name ?? null);
+            setLoading(false);
+
+
+        };
+
+        fetchSpotify();
+    });
+
+
+    return {displayName, loading};
+}
