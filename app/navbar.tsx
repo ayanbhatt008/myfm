06754cc00c4f1a1bfdfa315353e1418554421a37
ayanbhatt-MSI <ayan.bhatt008@gmail.com>
2025-12-03 23:03:12 -0500

    curr prog
    Signed-off-by: ayanbhatt-MSI <ayan.bhatt008@gmail.com>

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,7 +1,36 @@
 "use client"
 
 
+import {useEffect, useState} from "react";
+import {createClient} from "@/lib/supabase/client";
+
 export default function NavBar() {
+    const [isLoggedIn, setIsLoggedIn] = useState<null | "login" | "dashboard">(null);
+    const supabase = createClient();
+
+
+
+    useEffect(() => {
+        async function func() {
+            const {data: {user}, error} = await supabase.auth.getUser()
+
+
+                setIsLoggedIn(user ? "dashboard" : "login");
+        }
+
+        func();
+    }, [])
+
+    let authHref = null;
+    if (!isLoggedIn) {
+
+       authHref = (<a  className={"text-sm"}></a>)
+    }
+    else if (isLoggedIn == "login")
+        authHref = (<a href={"/login"} className={"text-sm"}> Login | Sign Up </a>)
+    else if (isLoggedIn == "dashboard")
+        authHref = (<a href={"/dashboard"} className={"text-sm"}> Dashboard </a>)
+
 
 
     return (
@@ -16,8 +45,8 @@ export default function NavBar() {
                     <a> About Us</a>
                 </div>
 
+                {authHref}
 
-                <a href={"/login"} className={"text-sm"}> Login </a>
 
 
             </div>
