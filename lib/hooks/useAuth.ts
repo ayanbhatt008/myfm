467289bbf2f1be734f96210fcaac1a800a433eb7
ayanbhatt-ACER <ayan.bhatt008@gmail.com>
2025-12-04 11:43:13 -0500

    useAuth hook

---
+++ lib/hooks/useAuth.ts
@@ -0,0 +1,29 @@
+import { supabase } from "../supabase/client";
+import { useEffect, useState } from "react";
+
+export function useAuth() {
+    const [user, setUser] = useState<any>(null);
+    const [loading, setLoading] = useState(true);
+
+
+    useEffect( () => {
+        supabase.auth.getUser()
+            .then(({data: {user}}) => {
+                setUser(user ?? null);
+                setLoading(false);
+            })
+
+        const {data: listener} = supabase.auth.onAuthStateChange((_event, session) => {
+        setUser(session?.user ?? null);
+        setLoading(false);
+        }); 
+
+        return () => listener.subscription.unsubscribe();
+    });
+
+    return {user, loading};
+
+
+
+    
+}
