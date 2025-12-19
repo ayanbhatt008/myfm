55d3e9846c18b8f2085b96f21ddd9750060d14e9
ayanbhatt-ACER <ayan.bhatt008@gmail.com>
2025-12-18 21:14:33 -0500

    basic implementation of a graph

--- app/recent-plays/analytics-page.tsx
+++ app/recent-plays/analytics-page.tsx
@@ -1,6 +1,8 @@
 import { tracksOnDay } from "@/lib/types/r2_types"
 import { possibleTabs } from "./history-tabs"
 import { analyticsProperties, useAnalyticsResult } from "./useAnalyticsResult"
+import { useEffect } from "react"
+import { AreaChart } from "@mantine/charts"
 
 interface Props {
     responseData: tracksOnDay[] | null
@@ -12,12 +14,29 @@ interface Props {
 export default function AnalyticsPage({responseData,  currentTab} : Props){
     const analytics: analyticsProperties | null = useAnalyticsResult(responseData, currentTab === 'analytics');
 
+    
+
     if (!analytics) 
         return (<div> not yet</div>);
 
+
+    
+
+    const ordered_track_counts = [...analytics.track_counts.entries()].sort((a, b) => b[1] - a[1])
+
+
     return (
-        <div>
-            {analytics.total_tracks}
+        <div className="m-10">
+            <AreaChart
+                h={300}
+                data={analytics.plays_per_day}
+                dataKey="date"
+                series={[
+                    {name: 'count', color: 'blue.6'}
+                ]}
+                curveType="natural"
+                
+            />
         </div>
     )
 }
--- app/recent-plays/useAnalyticsResult.ts
+++ app/recent-plays/useAnalyticsResult.ts
@@ -1,8 +1,14 @@
-import { tracksOnDay } from "@/lib/types/r2_types";
+import { r2Item, r2Track, tracksOnDay } from "@/lib/types/r2_types";
 import { useEffect, useState } from "react";
 
 export interface analyticsProperties {
-    total_tracks: number
+    total_tracks: number,
+    artist_counts: Map<string, number>,
+    track_counts: Map<string, number>,
+    plays_per_day: {
+        date: string,
+        count : number,
+    }[],
 }
 
 export function useAnalyticsResult(data : tracksOnDay[] | null, enabled : boolean) : analyticsProperties | null {
@@ -23,6 +29,7 @@ export function useAnalyticsResult(data : tracksOnDay[] | null, enabled : boolea
         const result : analyticsProperties | null = computeAnalytics(data);
         setAnalytics(result);
         
+        
 
     }, [data, enabled])
 
@@ -35,9 +42,38 @@ function computeAnalytics(data : null | tracksOnDay[]) : null | analyticsPropert
     if (!data)
         return null;
 
-    const total_tracks = data.reduce((sum, curr) => sum += curr.tracks.length, 0);
+    let total_tracks = 0;
+    const artist_counts = new Map<string, number>();
+    const track_counts = new Map<string, number>();
+    const plays_per_day : {date: string, count : number}[] = [];
+
+    data.forEach((day : tracksOnDay) => {
+
+        total_tracks += day.tracks.length;
+        
+        plays_per_day.push({
+            date: day.r2DateKey,
+            count: day.tracks.length
+        });
+        
+        day.tracks.forEach((track : r2Track) => {
+            track_counts.set(track.name, (track_counts.get(track.name) ?? 0) + 1);
+            
+            track.artists.forEach((artist : r2Item) => {
+                artist_counts.set(artist.name, (artist_counts.get(artist.name) ?? 0) + 1);
+            })
+        });
+
+
+    });
+    
 
     
 
-    return {total_tracks}
+    return {
+        total_tracks, 
+        artist_counts, 
+        track_counts, 
+        plays_per_day,
+    };
 }
